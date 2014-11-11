85a74a1bbef5c30fcfc1281fab95108210decb1e
Tim Licata <tim@timothylicata.com>
2014-11-11 11:40:12 -0800

    add authors#edit action and view (+authors#update)

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -6,4 +6,19 @@ class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
   end
+
+  def edit
+    @author = Author.find(params[:id])
+  end
+
+  def update
+    author = Author.find(params[:id])
+    author.update(get_author_params)
+    redirect_to authors_path
+  end
+
+  private
+  def get_author_params
+    params.require(:author).permit(:name)
+  end
 end
