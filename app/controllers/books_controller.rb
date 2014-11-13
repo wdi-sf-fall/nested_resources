7526e1ff2368005b7dfb76473ce7364d169f09a6
Tim Licata <tim@timothylicata.com>
2014-11-12 16:41:55 -0800

    add books#edit action and view (+books#update)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,19 @@ class BooksController < ApplicationController
     @author = Author.find(params[:author_id])
     @book = Book.find(params[:id])
   end
+  def edit
+    @author = Author.find(params[:author_id])
+    @book = Book.find(params[:id])
+  end
+  def update
+    author = Author.find(params[:author_id])
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to author_books_path(author)
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title)
+  end
 end
